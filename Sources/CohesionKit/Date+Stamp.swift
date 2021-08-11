08f47e742e4d92543c8ac0526400ce8a4d0dc427
pjechris <pjechris@users.noreply.github.com>
2021-08-11 20:56:48 +0200

    [src] added Stamp file

---
+++ Sources/CohesionKit/Date+Stamp.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+/// A type used to annotate track object modifications through time.
+/// Most of the time you'll just use date as stamp using `Date().stamp` method
+public typealias Stamp = Double
+
+extension Date {
+    /// Generate a stamp suitable to use in `IdentityMap`.
+    /// Don't suppose it equals unix timestamp (it is not)
+    public var stamp: Stamp {
+        timeIntervalSinceReferenceDate
+    }
+}
