c1ee0c2927aafffbe51068557010ee086f4f6235
pjechris <pjechris@users.noreply.github.com>
2021-09-25 12:09:18 +0200

    [src] make a relation object

--- Sources/CohesionKit/Relational.swift
+++ Sources/CohesionKit/Relational.swift
@@ -39,3 +39,34 @@ extension Relational {
             .eraseToAnyPublisher()
     }
 }
+
+public struct Relation<Element, ElementIdentity: Identifiable> {
+    /// key path to whose id will be used as `Element` identity
+    let primaryKeyPath: KeyPath<Element, ElementIdentity>
+    
+    var idKeyPath: KeyPath<Element, ElementIdentity.ID> { primaryKeyPath.appending(path: \.id) }
+    
+    /// object with their own identity contained in `Element` and that should be stored
+    /// apart (to track them idepedently)
+    let identities: [RelationKeyPath<Element>]
+    
+    /// create a new `Element` based on updates on item relations
+    let reduce: (KeyPathUpdates<Element>) -> Element
+    
+    public init(primaryKeyPath: KeyPath<Element, ElementIdentity>,
+                identities: [RelationKeyPath<Element>],
+                reduce: @escaping (KeyPathUpdates<Element>) -> Element) {
+        
+        self.primaryKeyPath = primaryKeyPath
+        self.identities = identities
+        self.reduce = reduce
+    }
+    
+    public init() where Element == ElementIdentity {
+        self.init(
+            primaryKeyPath: \Element.self,
+            identities: [.init(\Element.self)],
+            reduce: { $0[\Element.self] }
+        )
+    }
+}
