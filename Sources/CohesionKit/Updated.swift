581dae3ac697ebf64aea9807d9ff9bcc2cddb42e
jc_pastant_aircall <jc.pastant@aircall.io>
2022-01-14 19:02:41 +0100

    [src] update value using pointers

--- Sources/CohesionKit/Updated.swift
+++ Sources/CohesionKit/Updated.swift
@@ -8,11 +8,45 @@ public typealias Updater<Element> = (Updated<Element>) -> Element
 /// You can access updates using `Root` member names: `Updated<User>(..).username`
 @dynamicMemberLookup
 public struct Updated<Root> {
-    let root: Root
-    let updates: [AnyKeyPath: Any]
-    
-    /// return registered update or value from root
-    public subscript<T>(dynamicMember keyPath: KeyPath<Root, T>) -> T {
-        get { updates[keyPath] as? T ?? root[keyPath: keyPath] }
+  let root: Root
+  let updates: [AnyKeyPath: Any]
+
+  /// return registered update or value from root
+  public subscript<T>(dynamicMember keyPath: KeyPath<Root, T>) -> T {
+    get { updates[keyPath] as? T ?? root[keyPath: keyPath] }
+  }
+
+  static func reduce(root: Root, updates: [PartialKeyPath<Root>: Any]) -> Root {
+    var root = root
+
+    withUnsafeMutablePointer(to: &root) {
+      let pointer = UnsafeMutableRawPointer($0)
+
+      for (keyPath, value) in updates {
+        pointer.assign(value, to: keyPath)
+      }
     }
+
+    return root
+  }
+
+  static func assign<Value>(_: Value.Type) {
+
+  }
+}
+
+extension UnsafeMutableRawPointer {
+  func assign<Root>(_ value: Any, to keyPath: PartialKeyPath<Root>) {
+    func open<Value>(_: Value.Type) {
+      assign(value as! Value, to: unsafeDowncast(keyPath, to: KeyPath<Root, Value>.self))
+    }
+
+    _openExistential(type(of: keyPath).valueType, do: open)
+  }
+
+  func assign<Root, Value>(_ value: Value, to keyPath: KeyPath<Root, Value>) {
+    advanced(by: MemoryLayout<Root>.offset(of: keyPath)!)
+      .assumingMemoryBound(to: Value.self)
+      .pointee = value
+  }
 }
