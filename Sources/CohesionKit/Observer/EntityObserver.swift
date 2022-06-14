3577f1edec1ca080bbe2f553581d728276a3f2dc
pjechris <pjechris@users.noreply.github.com>
2022-06-14 14:13:11 +0200

    [identity] use queue for thread safety

--- Sources/CohesionKit/Observer/EntityObserver.swift
+++ Sources/CohesionKit/Observer/EntityObserver.swift
@@ -1,15 +1,23 @@
+import Foundation
+
 /// A type registering observers on a given entity from identity storage
 public struct EntityObserver<T>: Observer {
     let node: EntityNode<T>
+    let queue: DispatchQueue
     public let value: T
     
-    init(node: EntityNode<T>) {
+    init(node: EntityNode<T>, queue: DispatchQueue) {
+        self.queue = queue
         self.node = node
         self.value = node.value as! T
     }
     
     public func observe(onChange: @escaping (T) -> Void) -> Subscription {
-        let subscription = node.ref.addObserver(onChange: onChange)
+        let subscription = node.ref.addObserver { newValue in
+            queue.async {
+                onChange(newValue)
+            }
+        }
         let retain = Unmanaged.passRetained(node)
         
         return Subscription {
