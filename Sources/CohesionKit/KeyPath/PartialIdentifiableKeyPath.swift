954870bdc2811aa8f03a501b2db26df91a6f1c9d
pjechris <pjechris@users.noreply.github.com>
2023-01-02 17:34:56 +0100

    [keyPath] removed useless Hashable implementation

--- Sources/CohesionKit/KeyPath/PartialIdentifiableKeyPath.swift
+++ Sources/CohesionKit/KeyPath/PartialIdentifiableKeyPath.swift
@@ -65,13 +65,3 @@ public struct PartialIdentifiableKeyPath<Root> {
         }
     }
 }
-
-extension PartialIdentifiableKeyPath: Hashable {
-    public func hash(into hasher: inout Hasher) {
-        keyPath.hash(into: &hasher)
-    }
-
-    public static func ==(lhs: Self, rhs: Self) -> Bool {
-        lhs.keyPath == rhs.keyPath
-    }
-}
