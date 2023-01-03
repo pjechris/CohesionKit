70c5b108085b537fbfa442a5dd47bff66fa02220
pjechris <pjechris@users.noreply.github.com>
2023-01-03 14:03:01 +0100

    [combine] don't use some Publisher for now 🥲

--- Sources/CohesionKit/Combine/Publisher+CohesionKit.swift
+++ Sources/CohesionKit/Combine/Publisher+CohesionKit.swift
@@ -5,30 +5,34 @@ import Foundation
 extension Publisher {
   /// Stores the `Identifiable` upstream into an identityMap
   public func store(in identityMap: IdentityMap, named: AliasKey<Output>? = nil, modifiedAt: Stamp = Date().stamp)
-  -> some Publisher<Output, Failure> where Output: Identifiable {
+  -> AnyPublisher<Output, Failure> where Output: Identifiable {
     map { identityMap.store(entity: $0, named: named, modifiedAt: modifiedAt).asPublisher }
       .switchToLatest()
+      .eraseToAnyPublisher()
   }
 
   /// Stores the `Aggregate` upstream into an identityMap
   public func store(in identityMap: IdentityMap, named: AliasKey<Output>? = nil, modifiedAt: Stamp = Date().stamp)
-  -> some Publisher<Output, Failure> where Output: Aggregate {
+  -> AnyPublisher<Output, Failure> where Output: Aggregate {
     map { identityMap.store(entity: $0, named: named, modifiedAt: modifiedAt).asPublisher }
       .switchToLatest()
+      .eraseToAnyPublisher()
   }
 
   /// Stores the upstream collection into an identityMap
   public func store(in identityMap: IdentityMap, named: AliasKey<Output>? = nil, modifiedAt: Stamp = Date().stamp)
-  -> some Publisher<[Output.Element], Failure> where Output: Collection, Output.Element: Identifiable {
+  -> AnyPublisher<[Output.Element], Failure> where Output: Collection, Output.Element: Identifiable {
     map { identityMap.store(entities: $0, named: named, modifiedAt: modifiedAt).asPublisher }
       .switchToLatest()
+      .eraseToAnyPublisher()
   }
 
   /// Stores the upstream collection into an identityMap
   public func store(in identityMap: IdentityMap, named: AliasKey<Output>? = nil, modifiedAt: Stamp = Date().stamp)
-  -> some Publisher<[Output.Element], Failure> where Output: Collection, Output.Element: Aggregate {
+  -> AnyPublisher<[Output.Element], Failure> where Output: Collection, Output.Element: Aggregate {
     map { identityMap.store(entities: $0, named: named, modifiedAt: modifiedAt).asPublisher }
       .switchToLatest()
+      .eraseToAnyPublisher()
   }
 }
 
