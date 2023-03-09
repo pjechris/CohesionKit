c15d1bd83f33560e3bfdaa8f04c3fd19a8614396
pjechris <pjechris@users.noreply.github.com>
2023-03-09 13:35:30 +0100

    [#30] added tests showing the issue

--- Tests/CohesionKitTests/Observer/AliasObserverTests.swift
+++ Tests/CohesionKitTests/Observer/AliasObserverTests.swift
@@ -2,7 +2,7 @@ import XCTest
 @testable import CohesionKit
 
 class AliasObserverTests: XCTestCase {
-    func test_observe_refValueChanged_returnRefValue() {
+    func test_observe_refValueChanged_onChangeIsCalled() {
         let ref = Ref(value: Optional.some(EntityNode(SingleNodeFixture(id: 1), modifiedAt: 0)))
         let observer = AliasObserver(alias: ref, queue: .main)
         let newValue = SingleNodeFixture(id: 2)
@@ -51,13 +51,38 @@ class AliasObserverTests: XCTestCase {
         let observer = AliasObserver(alias: ref, queue: .main)
         let newValue = SingleNodeFixture(id: 3)
         var lastReceivedValue: SingleNodeFixture?
-        
+
         _ = observer.observe {
             lastReceivedValue = $0
         }
-        
+
         try node.updateEntity(newValue, modifiedAt: 1)
-        
+
         XCTAssertNil(lastReceivedValue)
+    }
+
+    func test_observeArray_oneElementChanged_onChangeIsCalled() throws {
+        let expectation = XCTestExpectation()
+        let entities = [
+            EntityNode(SingleNodeFixture(id: 1), modifiedAt: 0),
+            EntityNode(SingleNodeFixture(id: 2), modifiedAt: 0)
+        ]
+        let ref = Ref(value: Optional.some(entities))
+        let observer = AliasObserver(alias: ref, queue: .main)
+        let update = SingleNodeFixture(id: 1, primitive: "Update")
+        var lastObservedValue: [SingleNodeFixture]?
+
+        let subscription = observer.observe {
+            lastObservedValue = $0
+            expectation.fulfill()
+        }
+
+        try withExtendedLifetime(subscription) {
+            // try ref.value?[0].updateEntity(SingleNodeFixture(id: 1, primitive: "Update"), modifiedAt: 1)
+            try entities[0].updateEntity(SingleNodeFixture(id: 1, primitive: "Update"), modifiedAt: 1)
+
+            wait(for: [expectation], timeout: 1)
+            XCTAssertEqual(lastObservedValue?.first, update)
+        }
     }
 }
