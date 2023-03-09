f4430532a38b0c54f9a9788f6174fb3b660c6794
pjechris <pjechris@users.noreply.github.com>
2023-03-09 15:26:03 +0100

    fix(observer): [#30] Observe alias entity changes (#31)

---
+++ Tests/CohesionKitTests/Observer/EntityObserverTests.swift
@@ -0,0 +1,21 @@
+import XCTest
+@testable import CohesionKit
+
+class EntityObserverTests: XCTestCase {
+    func test_entityIsUpdated_onChangeIsCalled() throws {
+        let node = EntityNode(SingleNodeFixture(id: 0), modifiedAt: 0)
+        let observer = EntityObserver(node: node, queue: .main)
+        let newEntity = SingleNodeFixture(id: 0, primitive: "new entity version")
+        let expectation = XCTestExpectation()
+
+        let subscription = observer.observe {
+            expectation.fulfill()
+            XCTAssertEqual($0, newEntity)
+        }
+
+        try withExtendedLifetime(subscription) {
+            try node.updateEntity(newEntity, modifiedAt: 1)
+            wait(for: [expectation], timeout: 0.5)
+        }
+    }
+}
