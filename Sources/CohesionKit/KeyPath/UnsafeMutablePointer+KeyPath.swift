030f7f1c2ea3e73ef9eff3e3381a5b3ae274717b
pjechris <pjechris@users.noreply.github.com>
2023-05-13 17:31:25 +0200

    [#29] updated tests to use UnsafeMutablePointer

--- Sources/CohesionKit/KeyPath/UnsafeMutablePointer+KeyPath.swift
+++ Sources/CohesionKit/KeyPath/UnsafeMutablePointer+KeyPath.swift
@@ -1,26 +1,5 @@
 import Foundation
 
-extension UnsafeMutableRawPointer {
-    
-    func assign<Root, Value>(_ value: Value, to keyPath: KeyPath<Root, Value>) {
-        guard let pointer = UnsafeMutablePointer(mutating: self.assumingMemoryBound(to: Root.self).pointer(to: keyPath)) else {
-              fatalError("cannot update value for KeyPath<\(Root.self), \(Value.self)>. Computed properties are not supported.")
-        }
-
-        pointer.pointee = value
-    }
-    
-    func assign<Root, C>(_ value: C.Element, to keyPath: KeyPath<Root, C>, index: C.Index)
-      where C: MutableCollection, C.Index == Int {
-
-        guard let pointer = UnsafeMutablePointer(mutating: self.assumingMemoryBound(to: Root.self).pointer(to: keyPath)) else {
-              fatalError("cannot update value for KeyPath<\(Root.self), \(C.self)>. Computed properties are not supported.")
-        }
-
-        pointer.pointee.withContiguousMutableStorageIfAvailable { $0[index] = value }
-      }
-}
-
 extension UnsafeMutablePointer {
 
     func assign<Value>(_ value: Value, to keyPath: KeyPath<Pointee, Value>) {
--- Tests/CohesionKitTests/KeyPath/UnsafeMutablePointerTests.swift
+++ Tests/CohesionKitTests/KeyPath/UnsafeMutablePointerTests.swift
@@ -1,14 +1,12 @@
 import XCTest
 @testable import CohesionKit
 
-class UnsafeMutableRawPointerTests: XCTestCase {
+class UnsafeMutablePointerTests: XCTestCase {
     func test_assign_propertyIsImmutable_propertyIsChanged() {
         var hello = Hello()
 
         withUnsafeMutablePointer(to: &hello) {
-            let pointer = UnsafeMutableRawPointer($0)
-            
-            pointer.assign("world", to: \Hello.singleValue)
+            $0.assign("world", to: \Hello.singleValue)
         }
 
         XCTAssertEqual(hello.singleValue, "world")
@@ -17,10 +15,8 @@ class UnsafeMutableRawPointerTests: XCTestCase {
     func test_assign_keyPathIsCollection_propertyIsImmutable_collectionIsChangedAtIndex() {
         var hello = Hello()
 
-        withUnsafeMutablePointer(to: &hello) {
-            let pointer = UnsafeMutableRawPointer($0)
-            
-            pointer.assign(5, to: \Hello.collection, index: 3)
+        withUnsafeMutablePointer(to: &hello) {            
+            $0.assign(5, to: \Hello.collection, index: 3)
         }
         
         XCTAssertEqual(hello.collection, [1, 2, 3, 5])
