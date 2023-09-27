ef3e307f46078ec4a7f01d95779598e314ce4d27
pjechris <pjechris@users.noreply.github.com>
2023-09-27 18:02:19 +0200

    [store] store alias using AliasContainer

--- Sources/CohesionKit/Storage/AliasStorage.swift
+++ Sources/CohesionKit/Storage/AliasStorage.swift
@@ -1,48 +1,13 @@
 /// Keep a strong reference on each aliased node
-typealias AliasStorage = [AnyHashable: AnyObservable]
+typealias AliasStorage = [String: Any]
 
 extension AliasStorage {
-    subscript<T>(key: AliasKey<T>) -> Observable<EntityNode<T>?> {
-        mutating get {
-            if let store = self[AnyHashable(key)] as? Observable<EntityNode<T>?> {
-                return store
-            }
-
-            let store: Observable<EntityNode<T>?> = Observable(value: nil)
-            self[AnyHashable(key)] = store
-
-            return store
-
-        }
+    subscript<T>(_ type: T.Type, key aliasKey: AliasKey<T>) -> EntityNode<AliasContainer<T>>? {
+        get { self[key(for: T.self, key: aliasKey)] as? EntityNode<AliasContainer<T>> }
+        set { self[key(for: T.self, key: aliasKey)] = newValue }
     }
 
-    subscript<C: Collection>(key: AliasKey<C>) -> Observable<[EntityNode<C.Element>]?> {
-        mutating get {
-            if let store = self[AnyHashable(key)] as? Observable<[EntityNode<C.Element>]?> {
-                return store
-            }
-
-            let store: Observable<[EntityNode<C.Element>]?> = Observable(value: nil)
-            self[AnyHashable(key)] = store
-
-            return store
-
-        }
-    }
-
-    mutating func insert<T>(_ node: EntityNode<T>, key: AliasKey<T>) {
-        self[key].value = node
-    }
-
-    mutating func insert<C: Collection>(_ nodes: [EntityNode<C.Element>], key: AliasKey<C>) {
-        self[key].value = nodes
-    }
-
-    mutating func remove<T>(for key: AliasKey<T>) {
-        (self[AnyHashable(key)] as? Observable<EntityNode<T>?>)?.value = nil
-    }
-
-    mutating func remove<C: Collection>(for key: AliasKey<C>) {
-        (self[AnyHashable(key)] as? Observable<[EntityNode<C.Element>]?>)?.value = nil
+    private func key<T>(for type: T.Type, alias: AliasKey<T>) -> String {
+        "\(type):\(alias.name)"
     }
 }
