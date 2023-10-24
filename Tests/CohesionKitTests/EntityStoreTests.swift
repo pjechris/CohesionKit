41c846ba83f6f85c2758a226aa006aaa966b110c
pjechris <pjechris@users.noreply.github.com>
2023-10-24 18:34:03 +0200

    tech(cleaning): Rename IdentityMap and remove Observer protocol (#61)

--- Tests/CohesionKitTests/EntityStoreTests.swift
+++ Tests/CohesionKitTests/EntityStoreTests.swift
@@ -2,7 +2,7 @@ import XCTest
 @testable import CohesionKit
 
 // MARK: Store
-class IdentityMapTests: XCTestCase {
+class EntityStoreTests: XCTestCase {
     func test_storeAggregate_nestedEntitiesAreStored() {
         let entity = RootFixture(
             id: 1,
@@ -154,7 +154,7 @@ class IdentityMapTests: XCTestCase {
 }
 
 // MARK: Find
-extension IdentityMapTests {
+extension EntityStoreTests {
     func test_find_entityStored_noObserverAdded_returnNil() {
         let identityMap = IdentityMap()
         let entity = SingleNodeFixture(id: 1)
@@ -277,7 +277,7 @@ extension IdentityMapTests {
 
 // MARK: Update
 
-extension IdentityMapTests {
+extension EntityStoreTests {
 
     func test_update_entityIsAlreadyInserted_entityIsUpdated() {
         let identityMap = IdentityMap()
