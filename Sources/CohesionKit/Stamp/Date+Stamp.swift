2af3b19b9dfce6ea246e7f5f84347da3e00fe5fe
pjechris <pjechris@users.noreply.github.com>
2023-12-21 16:33:56 +0100

    [store] IdentityMap -> EntityStore

--- Sources/CohesionKit/Stamp/Date+Stamp.swift
+++ Sources/CohesionKit/Stamp/Date+Stamp.swift
@@ -5,7 +5,7 @@ import Foundation
 public typealias Stamp = Double
 
 extension Date {
-    /// Generate a stamp suitable to use in `IdentityMap`.
+    /// Generate a stamp suitable to use in `EntityStore`.
     /// Don't suppose it equals unix timestamp (it is not)
     public var stamp: Stamp {
         timeIntervalSinceReferenceDate
